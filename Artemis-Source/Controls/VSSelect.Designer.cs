dc3afe2a7763866a6bcda11c1008f2190c8b65d9
NFI-279 <78752610+NFI-279@users.noreply.github.com>
2024-07-11 14:59:31 +0300

    Add files via upload

---
+++ Artemis-Source/Controls/VSSelect.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Artemis_Loader
+{
+    partial class VSSelect
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(24)))), ((int)(((byte)(66)))));
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(23, 20);
+            this.panel3.Margin = new System.Windows.Forms.Padding(20);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Josefin Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(204)))), ((int)(((byte)(245)))));
+            this.button4.Location = new System.Drawing.Point(0, 47);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(3);
+            this.button4.Size = new System.Drawing.Size(200, 50);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Counter-Strike: Global Offensive [beta]";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Josefin Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(204)))), ((int)(((byte)(245)))));
+            this.button1.Location = new System.Drawing.Point(0, 3);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(3);
+            this.button1.Size = new System.Drawing.Size(200, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Counter-Strike: Global Offensive";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(24)))), ((int)(((byte)(66)))));
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Location = new System.Drawing.Point(23, 127);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 90);
+            this.panel4.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Josefin Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(204)))), ((int)(((byte)(245)))));
+            this.button3.Location = new System.Drawing.Point(3, 46);
+            this.button3.Margin = new System.Windows.Forms.Padding(15);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(3);
+            this.button3.Size = new System.Drawing.Size(81, 37);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Legacy";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Josefin Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(204)))), ((int)(((byte)(245)))));
+            this.button2.Location = new System.Drawing.Point(3, 4);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(3);
+            this.button2.Size = new System.Drawing.Size(81, 37);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Regular";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // VSSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(21)))), ((int)(((byte)(54)))));
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Name = "VSSelect";
+            this.Size = new System.Drawing.Size(501, 231);
+            this.Load += new System.EventHandler(this.VSSelect_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Artemis-Source/Controls/VSSelect.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Artemis_Loader
+{
+    public partial class VSSelect : UserControl
+    {
+        public VSSelect()
+        {
+            InitializeComponent();
+            button2.Visible = button3.Visible = false;
+            button2.Enabled = button3.Enabled = false;
+        }
+
+        private void VSSelect_Load(object sender, EventArgs e)
+        { }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Visible = true; button2.Enabled = true;
+            button3.Visible = true; button3.Enabled = true;
+            button1.ForeColor = Color.FromArgb(195, 23, 87);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.ForeColor = Color.FromArgb(195, 23, 87);
+            button3.ForeColor = Color.FromArgb(211, 204, 245);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button2.ForeColor = Color.FromArgb(211, 204, 245);
+            button3.ForeColor = Color.FromArgb(195, 23, 87);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.ForeColor = Color.FromArgb(195, 23, 87);
+            button1.ForeColor = Color.FromArgb(211, 204, 245);
+            button2.Visible = button2.Enabled = true;
+            button3.Visible = button3.Enabled = false;
+        }
+    }
+}
